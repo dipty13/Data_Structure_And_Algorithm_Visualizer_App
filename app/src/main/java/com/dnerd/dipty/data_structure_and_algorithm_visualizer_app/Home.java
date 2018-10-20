1ddbd9a1753dca171af13d22d9628eef143b8761
dipty13 <13dipty@gmail.com>
2018-10-20 18:21:06 +0600

    Added Initial Home page

---
+++ app/src/main/java/com/dnerd/dipty/data_structure_and_algorithm_visualizer_app/Home.java
@@ -0,0 +1,23 @@
+package com.dnerd.dipty.data_structure_and_algorithm_visualizer_app;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+        {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/dnerd/dipty/data_structure_and_algorithm_visualizer_app/MainActivity.java
+++ app/src/main/java/com/dnerd/dipty/data_structure_and_algorithm_visualizer_app/MainActivity.java
@@ -1,13 +1,25 @@
 package com.dnerd.dipty.data_structure_and_algorithm_visualizer_app;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,
+                                                         Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
