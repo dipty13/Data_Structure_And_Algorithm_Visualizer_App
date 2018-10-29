b435ad33e598c96d68ee57630849de27c9bfe3e8
dipty13 <13dipty@gmail.com>
2018-10-29 19:12:27 +0600

    Added more code for linked list

--- app/src/main/java/com/dnerd/dipty/data_structure_and_algorithm_visualizer_app/LinkedListVisualizer.java
+++ app/src/main/java/com/dnerd/dipty/data_structure_and_algorithm_visualizer_app/LinkedListVisualizer.java
@@ -14,62 +14,35 @@ import android.widget.LinearLayout;
 
 public class LinkedListVisualizer extends AppCompatActivity {
     private int[] mCodePic = new int[]{
-            R.drawable.code1,
-            R.drawable.code2,
-            R.drawable.code3,
-            R.drawable.code4,
-            R.drawable.code5,
-            R.drawable.code6,
-            R.drawable.code7,
-            R.drawable.code8,
-            R.drawable.code9,
-            R.drawable.code10,
-            R.drawable.code11,
-            R.drawable.code12,
-            R.drawable.code12i,
-            R.drawable.code13,
-            R.drawable.code14,
-            R.drawable.code15,
-            R.drawable.code16,
-            R.drawable.code17,
-            R.drawable.code18,
-            R.drawable.code19,
-            R.drawable.code20,
-            R.drawable.code21,
-            R.drawable.code22,
-            R.drawable.code23,
-            R.drawable.code24,
-            R.drawable.code25,
-            R.drawable.code26
+            R.drawable.code1, R.drawable.code2, R.drawable.code3,
+            R.drawable.code4, R.drawable.code5, R.drawable.code6,
+            R.drawable.code7,R.drawable.code8, R.drawable.code9,
+            R.drawable.code10, R.drawable.code11, R.drawable.code12,
+            R.drawable.code12i, R.drawable.code13, R.drawable.code14,
+            R.drawable.code15, R.drawable.code16, R.drawable.code17,
+            R.drawable.code18, R.drawable.code19, R.drawable.code20,
+            R.drawable.code21, R.drawable.code22, R.drawable.code23,
+            R.drawable.code24, R.drawable.code25, R.drawable.code26,
+            R.drawable.code27, R.drawable.code28, R.drawable.code29,
+            R.drawable.code30
     };
     private int[] mVisualizerPic = new int[]{
-            R.drawable.linked_list1,
-            R.drawable.linked_list2,
-            R.drawable.linked_list3,
-            R.drawable.linked_list4,
-            R.drawable.linked_list5,
-            R.drawable.linked_list6,
-            R.drawable.linked_list7,
-            R.drawable.linked_list8,
-            R.drawable.linked_list9,
-            R.drawable.linked_list10,
-            R.drawable.linked_list11,
-            R.drawable.linked_list12,
-            R.drawable.linked_list12i,
-            R.drawable.linked_list13,
-            R.drawable.linked_list14,
-            R.drawable.linked_list15,
-            R.drawable.linked_list16,
-            R.drawable.linked_list17,
-            R.drawable.linked_list18,
-            R.drawable.linked_list19,
-            R.drawable.linked_list20,
-            R.drawable.linked_list21,
-            R.drawable.linked_list22,
-            R.drawable.linked_list23,
-            R.drawable.linked_list24,
-            R.drawable.linked_list25,
-            R.drawable.linked_list26
+            R.drawable.linked_list1, R.drawable.linked_list2,
+            R.drawable.linked_list3, R.drawable.linked_list4,
+            R.drawable.linked_list5, R.drawable.linked_list6,
+            R.drawable.linked_list7, R.drawable.linked_list8,
+            R.drawable.linked_list9, R.drawable.linked_list10,
+            R.drawable.linked_list11,R.drawable.linked_list12,
+            R.drawable.linked_list12i, R.drawable.linked_list13,
+            R.drawable.linked_list14, R.drawable.linked_list15,
+            R.drawable.linked_list16, R.drawable.linked_list17,
+            R.drawable.linked_list18, R.drawable.linked_list19,
+            R.drawable.linked_list20, R.drawable.linked_list21,
+            R.drawable.linked_list22, R.drawable.linked_list23,
+            R.drawable.linked_list24, R.drawable.linked_list25,
+            R.drawable.linked_list26, R.drawable.linked_list27,
+            R.drawable.linked_list28, R.drawable.linked_list29,
+            R.drawable.linked_list30
     };
     private int index = 0;
     private int flag = 0;
@@ -102,7 +75,7 @@ public class LinkedListVisualizer extends AppCompatActivity {
         {
             mBackButton.setEnabled(false);
         }
-        if(index >= 26)
+        if(index >= 30)
         {
             mNextButton.setEnabled(false);
         }
@@ -113,15 +86,14 @@ public class LinkedListVisualizer extends AppCompatActivity {
                 public void onClick(View view) {
                     mBackButton.setEnabled(true);
                     index++;
-                    if(index == 26) {
+                    if(index == 30) {
                         mNextButton.setEnabled(false);
                     }
-                    if(index <= 26) {
+                    if(index <= 30) {
                         mCodeLayout.setBackground(getResources().getDrawable(mCodePic[index]));
                         mVisualizerLayout.setBackground(getResources().getDrawable(mVisualizerPic[index]));
                     }else{
                         mNextButton.setEnabled(false);
-                       // mBackButton.setEnabled(true);
                     }
 
                 }
